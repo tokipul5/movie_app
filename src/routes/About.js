811bb6a719320555c3dff3cdab582f2d241ab72c
Keeyou Kim <tokipul5@gmail.com>
2020-10-21 02:21:47 -0700

    Added navigation bar

---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>
+        I made this website with React.js because I love watching movies. The
+        contents are brought from API.
+      </span>
+      <span> Keeyou Kim</span>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/routes/Detail.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Detail.css";
+
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      //   return (<div><span>{location.state.title}</span></div>);
+      return (
+        <div className="detail__container">
+          <span>{location.state.title}</span>
+          <span>{location.state.summary}</span>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Detail;
